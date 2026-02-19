eba4cee5db0035c1f87aabf35c6fa4414d54ce59
glaubernascimento <glauber.nascimento@aluno.senac.rio>
2026-02-19 20:46:32 -0300

    ok

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCY6ZFgBpBUWgSMNb17EGLoIuOM5Xw-gG0",
+  authDomain: "projeto-flask-crud-a1fa1.firebaseapp.com",
+  projectId: "projeto-flask-crud-a1fa1",
+  storageBucket: "projeto-flask-crud-a1fa1.firebasestorage.app",
+  messagingSenderId: "237429861614",
+  appId: "1:237429861614:web:a48d3da890610a4fe357ff"
+};
