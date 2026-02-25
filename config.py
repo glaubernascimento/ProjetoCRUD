c2bb15c7a889f26d2f386769c0d914677df2d29b
glaubernascimento <glauber.nascimento@aluno.senac.rio>
2026-02-24 21:29:15 -0300

    ok

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py
+
+# Configurações do site / aplicativo
+APP = {
+    # Nome do site para a tag `<title>...</title>`
+    'title': 'MyPyPad',
+
+    # Nome / logo do site, em HTML, para a tag `.navbar-brand` e outros usos
+    'name': 'My<i class="bi bi-filetype-py text-warning px-0"></i>Pad',
+
+    # Chave secreta (48 caracteres)
+    # Obtenha essa chave rodando `python keygen.py`
+    'secret_key': 'e4e77e9bb7f88ad9e20a95a0b79b2ce8c6ec75d3cd01d7582ef37faf8a93808fa4eae7d0d6ae879a4f1902f9b21da2c7',
+}
+
+# Configurações do banco de dados
+DB = {}
+
+# Configurações dos cookies
+COOKIE = {}
