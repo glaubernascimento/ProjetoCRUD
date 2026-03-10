d472a7d3207b59b37e202a196bcd3155f7f7614d
glaubernascimento <glauber.nascimento@aluno.senac.rio>
2026-03-10 19:20:18 -0300

    ajustes

--- pages/owner.py
+++ pages/owner.py
@@ -1,4 +1,4 @@
-# pages/owner.py
+# pages\owner.py
 
 import sqlite3
 from flask import Blueprint, g, make_response, redirect, render_template, request, jsonify, url_for
@@ -13,7 +13,8 @@ def owner_login():
 
     # Recebe os dados do usuário, vindos do front-end, em JSON
     data = request.json
-    # print(data)  # Debug - Exibe o JSON que vem do front-end
+    # Debug - Exibe o JSON que vem do front-end
+    # print('\n\n\n', data, '\n\n\n')
 
     # Validação básica dos dados recebidos (ajuste conforme necessário)
     if not data or 'uid' not in data or 'email' not in data or 'createdAt' not in data or 'lastLoginAt' not in data:
@@ -25,23 +26,28 @@ def owner_login():
 
     # Verifica se o usuário já existe na tabela owners (baseado no UID do Firebase)
     cursor.execute(
-        'SELECT own_id FROM owners WHERE own_uid = ?', (data['uid'],))
+        'SELECT own_id FROM owners WHERE own_uid = ?',
+        (data['uid'],)
+    )
     # True → usuário existe; False = usuário não existe
     existing_user = cursor.fetchone()
 
     if existing_user:
-        # Atualiza os dados existentes (exceto created_at, que permanece o original)
+        # Atualiza os dados existentes
+        # Para segurança da informação, usa "prepared statement"
         cursor.execute('''
             UPDATE owners SET
                 own_display_name = ?,
                 own_email = ?,
                 own_photo_url = ?,
+                own_created_at = ?,
                 own_last_login_at = ?
             WHERE own_uid = ?
         ''', (
             data.get('displayName'),
             data.get('email'),
             data.get('photoURL'),
+            data.get('createdAt'),
             data.get('lastLoginAt'),
             data.get('uid')
         ))
